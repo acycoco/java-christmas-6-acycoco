da89f6685092fb5c7321c542973478c2de46ac93
acycoco <pink10257@naver.com>
2023-11-14 03:51:41 +0900

    feat(December): 주말인지 평일인지 특정날짜 전후인지 확인하는 기능추가

---
+++ src/main/java/christmas/domain/model/December.java
@@ -0,0 +1,55 @@
+package christmas.domain.model;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+
+public class December {
+
+    private static final int FIRST_DAY = 1;
+    private static final int LAST_DAY = 31;
+    private static final List<Integer> STAR_DAY = List.of(3, 10, 17, 24, 25, 31);
+
+    private final LocalDate date;
+
+    private December(int day) {
+        this.date = LocalDate.of(2023, 12, day);
+    }
+
+    public static December from(int day) {
+        validateRange(day);
+        return new December(day);
+    }
+
+    public boolean isWeekend() {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
+    }
+
+    public boolean isWeekday() {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return !isWeekend();
+    }
+
+    public boolean hasStar() {
+        return STAR_DAY.contains(date.getDayOfMonth());
+    }
+
+    public boolean isBefore(LocalDate specialDate) {
+        return date.isBefore(specialDate);
+    }
+
+    public boolean isAfter(LocalDate specialDate) {
+        return date.isAfter(specialDate);
+    }
+
+    public int getDay() {
+        return date.getDayOfMonth();
+    }
+
+    private static void validateRange(int day) {
+        if (day < FIRST_DAY || day > LAST_DAY) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
--- src/test/java/christmas/domain/model/DecemberTest.java
+++ src/test/java/christmas/domain/model/DecemberTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class DecemberCalendarTest {
+class DecemberTest {
 
     @DisplayName("달력에 별이 찍혀있으면 true를 반환한다.")
     @ParameterizedTest
     @ValueSource(ints = {3, 10, 17, 24, 25, 31})
     void hasStar(int starDay) {
         //given
-        DecemberCalendar decemberCalendar = DecemberCalendar.from(starDay);
+        December decemberCalendar = December.from(starDay);
 
         //when
         boolean hasStar = decemberCalendar.hasStar();
