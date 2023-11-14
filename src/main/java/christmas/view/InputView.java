46268ca256bb0b8b3fabb25608779a28f0f2f155
acycoco <pink10257@naver.com>
2023-11-15 04:43:41 +0900

    feat(InputView): 날짜를 입력받는 기능 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,19 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private final InputProcessor inputProcessor;
+
+    public InputView(InputProcessor inputProcessor) {
+        this.inputProcessor = inputProcessor;
+    }
+
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        return inputProcessor.toDate(input);
+    }
+
+}
