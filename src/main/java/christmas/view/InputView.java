b25512ea2c5509a5c1979ab92548478a799bd423
acycoco <pink10257@naver.com>
2023-11-15 04:45:58 +0900

    feat(InputView): 주문을 입력받는 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,9 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.model.OrderRequest;
+
+import java.util.List;
 
 public class InputView {
 
@@ -12,8 +15,18 @@ public class InputView {
 
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
+        String input = read();
         return inputProcessor.toDate(input);
+    }
+
+    public List<OrderRequest> readOrderRequests() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = read();
+        return inputProcessor.toOrderRequests(input);
+    }
+
+    private String read() {
+        return Console.readLine();
     }
 
 }
