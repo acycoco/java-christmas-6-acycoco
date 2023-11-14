6fd6b097d704fab74e125825c7451d2fb55f83b4
acycoco <pink10257@naver.com>
2023-11-15 01:46:48 +0900

    feat(EventType): 모든 이벤트를 관리하는 기능 추가

---
+++ src/main/java/christmas/domain/model/EventType.java
@@ -0,0 +1,30 @@
+package christmas.domain.model;
+
+public enum EventType {
+
+    CHRISTMAS_DDAY_DISCOUNT("크리스마스 디데이 할인", new ChristmasDdayEvent()),
+    WEEKDAY_DISCOUNT("평일 할인", new WeekdayEvent()),
+    WEEKEND_DISCOUNT("주말 할인", new WeekendEvent()),
+    SPECIAL_DISCOUNT("특별 할인", new SpecialEvent()),
+    GIFT_EVENT("증정 이벤트", new GiftEvent());
+
+    private final String name;
+    private final Event event;
+
+    EventType(String name, Event event) {
+        this.name = name;
+        this.event = event;
+    }
+
+    public boolean canDiscount(OrderInfo orderInfo) {
+        return event.canDiscount(orderInfo);
+    }
+
+    public Money discountAmount(OrderInfo orderInfo) {
+        return event.discountAmount(orderInfo);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
