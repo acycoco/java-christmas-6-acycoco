87b7faddb10f1c30b418f7de2676753894358e3e
acycoco <pink10257@naver.com>
2023-11-15 04:40:42 +0900

    feat(InputProcessor): 입력받은 주문 view에서의 유효성 검증 기능 추가

--- src/main/java/christmas/view/InputProcessor.java
+++ src/main/java/christmas/view/InputProcessor.java
@@ -2,8 +2,6 @@ package christmas.view;
 
 import christmas.domain.model.OrderRequest;
 import christmas.error.ErrorMessage;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +15,17 @@ public class InputProcessor {
         input = input.trim();
         return toNumber(input);
     }
+
+    public List<OrderRequest> toOrderRequests(String input) {
+        validateNotNull(input);
+        input = input.trim();
+
+        List<String> orderItems = parseWith(input, COMMA);
+        return orderItems.stream()
+                .map(orderItem -> parseOrderItem(orderItem, DASH))
+                .toList();
+    }
+
 
     private void validateNotNull(String input) {
         if (input == null) {
@@ -36,6 +45,21 @@ public class InputProcessor {
         return Arrays.stream(input.split(mark))
                 .map(String::trim)
                 .toList();
+    }
+
+    private OrderRequest parseOrderItem(String orderItem, String mark) {
+        List<String> orderItemParts = parseWith(orderItem, mark);
+        validateOrderItemPartsLength(orderItemParts);
+
+        String menuName = orderItemParts.get(0);
+        int quantity = Integer.parseInt(orderItemParts.get(1));
+        return new OrderRequest(menuName, quantity);
+    }
+
+    private void validateOrderItemPartsLength(List<String> orderItemParts) {
+        if (orderItemParts.size() != 2) {
+            throw new IllegalArgumentException("잘못된 입력 형식입니다: ");
+        }
     }
 
 }
--- src/test/java/christmas/view/InputProcessorTest.java
+++ src/test/java/christmas/view/InputProcessorTest.java
@@ -1,8 +1,11 @@
 package christmas.view;
 
+import christmas.domain.model.OrderRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -31,4 +34,37 @@ class InputProcessorTest {
         assertThatThrownBy(() -> inputProcessor.toDate(",13 "))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+
+    @DisplayName("주문을 orderRequests로 변환하는 데 성공한다.")
+    @Test
+    void totoOrderRequestsSuccess() {
+        List<OrderRequest> orderRequests = inputProcessor.toOrderRequests("타파스 - 1, 제로콜라-2  ");
+
+        assertThat(orderRequests.size())
+                .isEqualTo(2);
+        assertThat(orderRequests.get(0).getMenuName())
+                .isEqualTo("타파스");
+        assertThat(orderRequests.get(0).getQuantity())
+                .isEqualTo(1);
+        assertThat(orderRequests.get(1).getMenuName())
+                .isEqualTo("제로콜라");
+        assertThat(orderRequests.get(1).getQuantity())
+                .isEqualTo(2);
+    }
+
+    @DisplayName("주문개수에 문자가 들어있어서 orderRequests로 변환하는 데 실패한다.")
+    @Test
+    void toOrderRequestFailIfContainString() {
+        assertThatThrownBy(() -> inputProcessor.toOrderRequests("타파스-1,제로콜라-b  "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문하나가 메뉴, 개수말고 다른 게 포함된 경우 orderRequests로 변환하는 데 실패한다.")
+    @Test
+    void toOrderRequestFailIfLengthIsNotTwo() {
+        assertThatThrownBy(() -> inputProcessor.toOrderRequests("타파스-1-2,제로콜라-1  "))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
