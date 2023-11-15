b3581429dc825f1ec0a2c2b14da0bb72f4c12ca2
acycoco <pink10257@naver.com>
2023-11-15 18:07:06 +0900

    refactor(OutputView): 출력 요구사항에 맞도록 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,25 +5,28 @@ import christmas.dto.GiftMenuDto;
 import christmas.dto.MoneyDto;
 import christmas.dto.OrderDto;
 import christmas.dto.EventBadgeDto;
+import christmas.exception.ErrorMessage;
+
 import java.math.BigDecimal;
 import java.util.Map;
 
 public class OutputView {
 
-    private static final String ORDER_MENU_PROMPT = "<주문메뉴>\n";
-    private static final String TOTAL_ORDER_PRICE_PROMPT = "<할인 전 총주문 금액>\n";
-    private static final String GIFT_MENU_PROMPT = "<증정 메뉴>\n";
-    private static final String BENEFIT_DETAILS_PROMPT = "<혜택 내역>\n";
-    private static final String TOTAL_BENEFIT_AMOUNTS_PROMPT = "<총혜택 금액>\n";
-    private static final String EXPECTED_PAYMENT_PROMPT = "<할인 후 예상 결제 금액>\n";
-    private static final String EVENT_BADGE_PROMPT = "<12월 이벤트 배지>\n";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String ORDER_MENU_PROMPT = "<주문 메뉴>" + LINE_SEPARATOR;
+    private static final String TOTAL_ORDER_PRICE_PROMPT = "<할인 전 총주문 금액>" + LINE_SEPARATOR;
+    private static final String GIFT_MENU_PROMPT = "<증정 메뉴>" + LINE_SEPARATOR;
+    private static final String BENEFIT_DETAILS_PROMPT = "<혜택 내역>" + LINE_SEPARATOR;
+    private static final String TOTAL_BENEFIT_AMOUNTS_PROMPT = "<총혜택 금액>" + LINE_SEPARATOR;
+    private static final String EXPECTED_PAYMENT_PROMPT = "<할인 후 예상 결제 금액>" + LINE_SEPARATOR;
+    private static final String EVENT_BADGE_PROMPT = "<12월 이벤트 배지>" + LINE_SEPARATOR;
 
     public void printOrderMenu(OrderDto orderDto) {
         StringBuilder line = new StringBuilder(ORDER_MENU_PROMPT);
         Map<String, Integer> orderItems = orderDto.getOrderItems();
 
         orderItems.forEach((menuName, quantity) -> {
-            line.append(menuName).append(" ").append(quantity).append("개\n");
+            line.append(menuName).append(" ").append(quantity).append("개" + LINE_SEPARATOR);
         });
 
         System.out.println(line);
@@ -32,7 +35,7 @@ public class OutputView {
     public void printTotalOrderPrice(MoneyDto totalOrderPrice) {
         StringBuilder line = new StringBuilder(TOTAL_ORDER_PRICE_PROMPT);
         line.append(formatMoney(totalOrderPrice.getMoney()));
-        System.out.println(line);
+        System.out.println(line + LINE_SEPARATOR);
     }
 
     public void printGiftMenu(GiftMenuDto giftMenuDto) {
@@ -41,12 +44,12 @@ public class OutputView {
         Map<String, Integer> giftMenus = giftMenuDto.getGiftMenu();
 
         if (giftMenus.isEmpty()) {
-            line.append("없음");
+            line.append("없음" + LINE_SEPARATOR);
         }
 
         if (!giftMenus.isEmpty()) {
             giftMenus.forEach((menuName, quantity) -> {
-                line.append(menuName).append(" ").append(quantity).append("개\n");
+                line.append(menuName).append(" ").append(quantity).append("개" + LINE_SEPARATOR);
             });
         }
 
@@ -59,12 +62,12 @@ public class OutputView {
         Map<String, BigDecimal> detailsDetails = benefitDetailsDto.getDetails();
 
         if (detailsDetails.isEmpty()) {
-            line.append("없음");
+            line.append("없음" + LINE_SEPARATOR);
         }
 
         if (!detailsDetails.isEmpty()) {
             detailsDetails.forEach((eventName, money) -> {
-                line.append(eventName).append(": ").append(formatMinusMoney(money)).append("원\n");
+                line.append(eventName).append(": ").append(formatMinusMoney(money)).append(LINE_SEPARATOR);
             });
         }
 
@@ -73,20 +76,34 @@ public class OutputView {
 
     public void printTotalBenefitAmounts(MoneyDto totalBenefitAmounts) {
         StringBuilder line = new StringBuilder(TOTAL_BENEFIT_AMOUNTS_PROMPT);
-        line.append(formatMinusMoney(totalBenefitAmounts.getMoney()));
-        System.out.println(line);
+        BigDecimal totalBenefitAmountsMoney = totalBenefitAmounts.getMoney();
+        if (totalBenefitAmountsMoney.equals(BigDecimal.ZERO)) {
+            line.append("없음");
+        }
+        else {
+            line.append(formatMinusMoney(totalBenefitAmounts.getMoney()));
+        }
+        System.out.println(line + LINE_SEPARATOR);
     }
 
     public void printExpectedPayment(MoneyDto expectedPayment) {
         StringBuilder line = new StringBuilder(EXPECTED_PAYMENT_PROMPT);
         line.append(formatMoney(expectedPayment.getMoney()));
-        System.out.println(line);
+        System.out.println(line + LINE_SEPARATOR);
     }
 
     public void printEventBadge(EventBadgeDto eventBadgeDto) {
         StringBuilder line = new StringBuilder(EVENT_BADGE_PROMPT);
         line.append(eventBadgeDto.getBadgeName());
-        System.out.println(line);
+        System.out.println(line + LINE_SEPARATOR);
+    }
+
+    public void printErrorMessage(ErrorMessage errorMessage) {
+        System.out.println(errorMessage.getMessage());
+    }
+
+    public void printEmptyLine() {
+        System.out.println();
     }
 
     private String formatMoney(BigDecimal money) {
