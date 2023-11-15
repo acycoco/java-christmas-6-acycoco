4f2470199fbeb0f7bd762969fe8e832cb0899eb4
acycoco <pink10257@naver.com>
2023-11-15 18:05:22 +0900

    feat(Application): 프로그램 실행하는 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,17 @@
 package christmas;
 
+import christmas.controller.DecemberEventController;
+import christmas.view.InputProcessor;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView(new InputProcessor());
+        OutputView outputView = new OutputView();
+
+        DecemberEventController decemberEventController = new DecemberEventController(inputView, outputView);
+        decemberEventController.play();
     }
 }
